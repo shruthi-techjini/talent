d96ed316aa4370a436f188f9921c78891dfe4292
shruthi-techinni <shruthi.r@techjini.com>
2018-02-10 13:58:55 +0530

    post create

--- src/AppBundle/Constants/Constants.php
+++ src/AppBundle/Constants/Constants.php
@@ -7,4 +7,11 @@ class Constants{
 	Const USER_STATUS_VERIFIED = 2;
 	
 	Const USER_TYPE_USER = 1;
+	Const USER_TYPE_TALENT = 2;
+	Const USER_TYPE_ADMIN = 3;
+	
+	Const POST_STATUS_PENDING = 1;
+	Const POST_STATUS_ACTIVE = 2;
+	Const POST_STATUS_INACTIVE = 3;
+	Const POST_STATUS_DELETED = 4;
 }
